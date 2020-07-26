ea6dc5b7193d4bf147337bb2692a74d374d3abbf
abdullahsshaker10 <abdullah.shaker2017@gmail.com>
2020-07-26 23:54:21 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from'./components/navBar';
+import Counters from './components/counters'
+
+class App extends Component {
+    
+  state = { 
+      counters:[
+          {id : 1, value : 4},
+          {id : 2, value : 0},
+          {id : 3, value : 0},
+          {id : 4, value : 0},
+
+      ]
+  }
+
+  handelIncrement = counter =>{
+      const counters = [...this.state.counters];
+      const index = counters.indexOf(counter);
+      counters[index] = {...counter};
+      counters[index].value++;
+      console.log(counters)
+      this.setState({counters});
+  }
+
+  handelReset = () =>{
+      const counters = this.state.counters.map( c => {
+          c.value = 0;
+          return c;
+      });
+
+      this.setState({counters});
+
+  }
+  handelDelete = counterId =>{
+      const counters = this.state.counters.filter(c =>c.id !== counterId);
+      this.setState({counters:counters});
+  }
+    
+  
+  
+  render() { 
+
+    return (
+      <React.Fragment>
+        <NavBar totalCounters = {this.state.counters.filter(c => c.value > 0).length}/>
+        <main className='cotainer'>
+          <Counters 
+          onReset = {this.handelReset} 
+          onDelete = {this.handelDelete}
+          onIncrement = {this.handelIncrement}
+          counters = {this.state.counters}
+           />
+        </main>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
